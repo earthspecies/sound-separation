39a1321039f79ad3682bd2220e12299b3f18e68b
Name <miron.marius@gmail.com>
2023-03-30 16:32:52 +0200

    trying to get yamnet inference on the separated output

--- ESP/zebra_finch_interactive_playbacks/mixit_separation.py
+++ ESP/zebra_finch_interactive_playbacks/mixit_separation.py
@@ -8,23 +8,31 @@ import pandas as pd
 import numpy as np
 import scipy
 import librosa
+import csv
 import soundfile as sf
 from io import StringIO
+
+import pcen_snr
 
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 import sklearn
-from sklearn import cluster
+from sklearn import cluster, pipeline
 
-import tensorflow as tf
+import tensorflow as tf2
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
+tf.compat.v1.enable_eager_execution()
+
+import tensorflow_hub
 
 
 from contextlib import redirect_stdout
 
 MIN_FILE_SIZE = 1000
+ONSET_THRESHOLD = 4e-4
+NSOURCES = 4
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -35,21 +43,26 @@ parser.add_argument(
     "--model_dir", type=str, required=True, help="Local path to mixit model directory"
 )
 
-def SNR(signal, noise):
-    signal_mean=np.mean(10**signal)
-    noise_mean=np.mean(10**noise)
-    SNR = 10*np.log10(abs(signal_mean-noise_mean)/(noise_mean+1e-10))
-    #print(str(round(SNR,2))+' '+'dB')
-    return SNR
 
-def signaltonoise(a, axis=0, ddof=0):
-    a = np.asanyarray(a)
-    m = a.mean(axis)
-    sd = a.std(axis=axis, ddof=ddof)
-    return np.where(sd == 0, 0, m/sd)
+# Find the name of the class with the top score when mean-aggregated across frames.
+def class_names_from_csv(class_map_csv_text):
+    """Returns list of class names corresponding to score vector."""
+    class_names = []
+    with tf.io.gfile.GFile(class_map_csv_text) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            class_names.append(row['display_name'])
 
+    return class_names
+
+@tf.function
+def compute(model, audio):
+  scores, _, _ = model(audio)
+  return scores
+
+@tf.function
 def main(conf):
-    nsources = 4
+   
     model_dict = {4:'3223090',8:'2178900'}
 
     if os.path.isdir(conf["dataset_path"]):
@@ -74,14 +87,21 @@ def main(conf):
     #load tf mixit model
     graph = tf.Graph()
     sess = tf.Session(graph=graph)
-    meta_graph_filename = os.path.join(conf["model_dir"], 'output_sources'+str(nsources), 'inference.meta')
+    meta_graph_filename = os.path.join(conf["model_dir"], 'output_sources'+str(NSOURCES), 'inference.meta')
     tf.logging.info('Importing meta graph: %s', meta_graph_filename)
     with graph.as_default() as g:
         saver = tf.train.import_meta_graph(meta_graph_filename)
         meta_graph_def = g.as_graph_def()
+    
+    graph1 = tf.Graph()
+    sess1 = tf.compat.v1.Session(graph=graph1)
+    yamnet_model = tensorflow_hub.load('https://tfhub.dev/google/yamnet/1')
+    # class_map_path = yamnet_model.class_map_path().numpy()
+    # class_names = class_names_from_csv(class_map_path)
 
-    with sess as ss:
-        saver.restore(ss, os.path.join(conf["model_dir"],"output_sources"+str(nsources),"model.ckpt-"+model_dict[nsources]))
+    #tf.compat.v1.disable_eager_execution()
+    with sess as ss, sess1 as ss1:
+        saver.restore(ss, os.path.join(conf["model_dir"],"output_sources"+str(NSOURCES),"model.ckpt-"+model_dict[NSOURCES]))
     
         input_tensor_name = 'input_audio/receiver_audio:0'
         output_tensor_name = 'denoised_waveforms:0'
@@ -90,53 +110,97 @@ def main(conf):
         mask_tensor = graph.get_tensor_by_name('Reshape_2:0') #spectrograms
         #mask_tensor = graph.get_tensor_by_name('mask:0') #masks 
         #get network parameters
-        #[n.name for n in tf.get_default_graph().as_graph_def().node]
+        # import pdb;pdb.set_trace()
+        # pars = [n.name for n in tf.get_default_graph().as_graph_def().node if hasattr(n, 'op') and n.op == 'Const']
+        
 
-        #segmentation and grouping of short events 
+        # md.sort_values(by='time',inplace=True)
+        drop_first = True
         for index, row in md.iterrows():
+            # if drop_first:
+            #     md.drop([index],inplace=True) #drop first 30 seconds because it's an outlier - recording setup
+            #     drop_first = False
+            #     continue
             timestamp = row['timestamp']
-            for c,p in {'Left':row['left_path'],'Right':row['right_path']}.items():
+            onsets = []
+            audio = []
+            mini_dict = {'Left':row['left_path'],'Right':row['right_path']}
+            for c,p in mini_dict.items():
                 input_mix,sample_rate = librosa.load(os.path.join(conf["dataset_path"],c,p))
                 # ### DC offset removal
-                # freq_cut=150 #Hz - higher frequency cutoff since these are bird sounds
-                # [b,a] = scipy.signal.butter(2, freq_cut/(sample_rate/2), 'highpass')
-                # input_wav = scipy.signal.filtfilt(b,a,input_mix)
+                lowcut=200 #Hz - higher frequency cutoff since these are bird sounds
+                highcut=10000 #Hz - higher frequency cutoff since these are bird sounds
+                [b,a] = scipy.signal.butter(4,[lowcut, highcut], fs=sample_rate, btype='band')
+                input_wav = scipy.signal.lfilter(b,a,input_mix)
+                
+                # # rms is not a good measure because some signals have low amplitude while air conditioning ones have high amplitude
+                # rms = librosa.feature.rms(S=librosa.magphase(librosa.stft(input_wav, window=np.ones, center=False))[0])
+                # # zero crossing rate is not very stable, noise can have high zcr
+                # zcr = librosa.feature.zero_crossing_rate(y=input_wav)
+                # # noise may have low spectral flatness but so do some signals with sparse calls
+                # sp = librosa.feature.spectral_flatness(S=librosa.feature.melspectrogram(y=input_wav, sr=sample_rate, n_mels=128, fmin=1000, fmax=10000, hop_length=512))
+                # spectral flux onset seems to be the best feature but the threshold might be problematic with the sparse calls
+                on = librosa.onset.onset_strength(S=librosa.feature.melspectrogram(y=input_wav, sr=sample_rate, n_mels=128, fmin=1000, fmax=10000, hop_length=512))
+                onsets.append(np.mean(on))
+                audio.append(input_wav)
+            if onsets[0]<ONSET_THRESHOLD and onsets[1]<ONSET_THRESHOLD:
+                print("Skipping "+timestamp+" because both channels have low onset strength.")
+                continue
+
+
+                # ### save audio
+                # sf.write(os.path.join(conf["save_dir"],'{}-{}.wav'.format(timestamp,p.split('_',1)[0])),np.squeeze(input_wav), sample_rate, 'PCM_24')
+                # for s in range(NSOURCES):
+                #     sf.write(os.path.join(conf["save_dir"],'{}-{}-source{}.wav'.format(timestamp,p.split('_',1)[0],s+1)), separated_waveforms[s], sample_rate, 'PCM_24')
+              
+           
+            centroids = 'k-means++'
+            for i,c in enumerate(mini_dict):
+                p = mini_dict[c]
                 #import pdb;pdb.set_trace()
-                ### feed forward audio through network
-                input_wav = input_mix[np.newaxis,np.newaxis,:]
-                separated_waveforms = sess.run(
-                    output_tensor,
-                    feed_dict={input_placeholder: input_wav})[0]
-                sf.write(os.path.join(conf["save_dir"],'{}-{}.wav'.format(timestamp,p.split('_',1)[0])),np.squeeze(input_wav), sample_rate, 'PCM_24')
-                # features = np.zeros((4, 4))
-                # featuresc = np.zeros((4, 4))
-                for s in range(nsources):
-                    sf.write(os.path.join(conf["save_dir"],'{}-{}-source{}.wav'.format(timestamp,p.split('_',1)[0],s+1)), separated_waveforms[s], sample_rate, 'PCM_24')
-                #     features[s,0] = SNR(separated_waveforms[s],np.squeeze(input_wav))
-                #     featuresc[s,0] = np.correlate(separated_waveforms[s],np.squeeze(input_wav),'full').argmax()/len(separated_waveforms[s])
-                #     features[s,1:4] = np.array([SNR(separated_waveforms[s],separated_waveforms[s1]) for s1 in range(4) if s1 != s])
-                #     featuresc[s,1:4] = np.array([np.correlate(separated_waveforms[s],separated_waveforms[s1], 'full').argmax()/len(separated_waveforms[s]) for s1 in range(4) if s1 != s])
-                # km = sklearn.cluster.KMeans(n_clusters=2, init='k-means++', n_init=30, max_iter=100, random_state=42)
-                # km.fit(features)
-                # print(km.labels_)
-                # km = sklearn.cluster.KMeans(n_clusters=2, init='k-means++', n_init=30, max_iter=100, random_state=42)
-                # km.fit(featuresc)
-                # print(km.labels_)
+                input_wav = audio[i][np.newaxis,np.newaxis,:]
                 # masks = sess.run(
                 #     mask_tensor,
                 #     feed_dict={input_placeholder: input_wav})[0]
-                # masks_r = masks.reshape((nsources, -1))
-                # # ica = sklearn.decomposition.FastICA(n_components=masks.shape[1],random_state=42,whiten='unit-variance')
-                # # ica.fit(masks_r)
-                # # X = ica.transform(masks_r)
-                # # pca = sklearn.decomposition.SparsePCA(n_components=100,random_state=42)
-                # # pca.fit(masks_r)
-                # # X = pca.transform(masks_r)
-                # km = sklearn.cluster.KMeans(n_clusters=2, init='k-means++', n_init=30, max_iter=100, random_state=42)
-                # km.fit(masks_r)
+                # masks_r = masks.reshape((NSOURCES, -1))
+                # pca = sklearn.decomposition.PCA(n_components=NSOURCES,random_state=42)
+                # km = sklearn.cluster.KMeans(n_clusters=2, init=centroids, n_init=30, max_iter=100, random_state=42)
+                # pipe = sklearn.pipeline.make_pipeline(sklearn.preprocessing.Normalizer(), pca, km)
+                # pipe.fit(masks_r)
                 # print(km.labels_)
-                # if km.labels_.sum() == 2:
-                #     import pdb;pdb.set_trace()
+                # #centroids  = km.cluster_centers_ 
+                # if km.labels_.sum() == 1:
+                #     audio_id =  [index for index,value in enumerate(km.labels_) if value == 1][0]  
+                # elif km.labels_.sum() == NSOURCES-1:
+                #     audio_id =  [index for index,value in enumerate(1-km.labels_) if value == 1][0]
+                # else:
+                #     import pdb;pdb.set_trace()  
+                ### all together now
+                separated_waveforms = sess.run(
+                    output_tensor,
+                    feed_dict={input_placeholder: input_wav})[0]
+                
+                for waveform in separated_waveforms:
+                    waveform = waveform / tf.int16.max
+                    
+                    import pdb;pdb.set_trace()
+
+                    outs = ss1.run(compute(yamnet_model,waveform), 
+                        feed_dict={input_placeholder: waveform})
+                    scores, _, _ = yamnet_model(waveform)
+                    scores_np = scores.numpy()
+                    infered_class = class_names[scores_np.mean(axis=0).argmax()]
+                    print(f'The main sound is: {infered_class}')
+
+
+
+                
+                # clean = separated_waveforms[audio_id]
+                # noise = np.sum(separated_waveforms[[index for index in range(NSOURCES) if index!=audio_id]],axis=0) 
+                # sf.write(os.path.join(conf["save_dir"],'{}-{}.wav'.format(timestamp,p.split('_',1)[0])),np.squeeze(input_wav), sample_rate, 'PCM_24')
+                # sf.write(os.path.join(conf["save_dir"],'{}-{}-clean.wav'.format(timestamp,p.split('_',1)[0])),np.squeeze(clean), sample_rate, 'PCM_24')    
+                # sf.write(os.path.join(conf["save_dir"],'{}-{}-noise.wav'.format(timestamp,p.split('_',1)[0])),np.squeeze(noise), sample_rate, 'PCM_24')            
+               
 
   
 
