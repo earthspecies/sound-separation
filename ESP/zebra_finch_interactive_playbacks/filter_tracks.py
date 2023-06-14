5e7549ab6e946cfc349a07308310088488357464
Name <miron.marius@gmail.com>
2023-06-14 13:02:07 +0200

    update

--- ESP/zebra_finch_interactive_playbacks/filter_tracks.py
+++ ESP/zebra_finch_interactive_playbacks/filter_tracks.py
@@ -127,106 +127,106 @@ def main(conf):
     ### yamnet
     with tf.device('/CPU:0'):
         model = hub.load('https://tfhub.dev/google/yamnet/1')
-    class_map_path = model.class_map_path().numpy()
-    class_names = class_names_from_csv(class_map_path)
-    bird_ids = [i for i,c in enumerate(class_names) if c in CLASSES.values()]
+        class_map_path = model.class_map_path().numpy()
+        class_names = class_names_from_csv(class_map_path)
+        bird_ids = [i for i,c in enumerate(class_names) if c in CLASSES.values()]
 
 
-    md = pd.read_csv(os.path.join(conf["dataset_path"],'separation','metadata.csv'))
-    filtered_md = pd.DataFrame(columns=["timestamp","time","ID_left","ID_right","sep_path","bird","noise","left_csv","right_csv"])
-    
-    for index, row in md.iterrows():
-        audio = []
-        mini_dict = {'Left':row['ID_left'],'Right':row['ID_right']}
-        if not os.path.exists(row["sep_path"]+'-'+row['ID_left']+'-source4.wav') or not os.path.exists(row["sep_path"]+'-'+row['ID_right']+'-source4.wav') :
-            continue
-        for i,c in enumerate(mini_dict):
-            p = mini_dict[c]
-            for s in range(1,NSOURCES+1):
-                input_mix,sample_rate = librosa.load(row["sep_path"]+'-'+p+'-source'+str(s)+'.wav',sr=None)
-                if  i==0 and s==1:
-                    audio = np.zeros((2,NSOURCES,len(input_mix)))
-                    stats = np.zeros((2,NSOURCES,2))
-                if len(input_mix) < len(audio[i,s-1,:]):
-                    audio[i,s-1,:len(input_mix)] = input_mix
-                elif len(input_mix) > len(audio[i,s-1,:]):
-                    audio[i,s-1,:] = input_mix[:len(audio[i,s-1,:])]
-                else:
-                    audio[i,s-1,:] = input_mix
-                waveform = librosa.util.normalize(input_mix) 
-                scores, _, _ = model(waveform)
-                scores_np = scores.numpy()
-                scores_birds = scores_np[:,bird_ids]
-                stats[0,s-1,i] = np.max(scores_birds)
-                stats[1,s-1,i] = np.mean(scores_birds)
-         
-        ### at least one prediction is above threshold and its mean is above threshold (more than a single vocalization)
-        if np.any(np.logical_and(stats[0,:,:]>0.9, stats[1,:,:]>0.01)):
-            ### avoid having bird predictions in the majority of tracks - cases where mixit fails
-            if np.all((np.logical_and(stats[0,:,:]>0.9, stats[1,:,:]>0.01)).sum(axis=0)<NSOURCES//2):
-                pred_bird = np.zeros((2, len(audio[0,0,:])))
-                pred_noise = np.zeros((2, len(audio[0,0,:])))
-                for i in range(2):
-                    max_max = stats[0,:,i].max() 
-                    argmax_max = stats[0,:,i].argmax()
-                    for s in range(NSOURCES):
-                        if stats[0,s,i] == max_max:
-                            pred_bird[i,:] += audio[i,s,:]
-                        else:
-                            pred_noise[i,:] += audio[i,s,:]
-                if WIENER:
-                    pred_bird, pred_noise = wiener_filter(pred_bird, pred_noise)            
-                    
-                sf.write(os.path.join(conf["save_dir"],'{}-{}-{}-bird.wav'.format(row['timestamp'], row['ID_left'], row['ID_right'])), pred_bird.transpose(), sample_rate, 'PCM_24')
-                sf.write(os.path.join(conf["save_dir"],'{}-{}-{}-noise.wav'.format(row['timestamp'], row['ID_left'], row['ID_right'])), pred_noise.transpose(), sample_rate, 'PCM_24')
-                frames2time = 512/sample_rate
-                rms = librosa.feature.rms(S=librosa.magphase(librosa.stft(pred_bird, window=np.ones, center=False))[0]).squeeze()
-                rms[0] = librosa.util.normalize(rms[0]) 
-                rms[1] = librosa.util.normalize(rms[1]) 
-                rms = scipy.ndimage.uniform_filter1d(rms, size=5)
-                activity, start, end = [], [], []
-                for i in range(2):
-                    a, s, e = threshold_activity(rms[i], 0.01 , 0.1)
-                    activity.append(a)
-                    start.append(s)
-                    end.append(e)
-               
-                # fig, ax = plt.subplots(figsize=(10,6))
-                # ax.plot(rms[0], label='left',color='green')
-                # ax.plot(rms[1], label='right',color='red')
-                # colors = ['green','red']
+        md = pd.read_csv(os.path.join(conf["dataset_path"],'separation','metadata.csv'))
+        filtered_md = pd.DataFrame(columns=["timestamp","time","ID_left","ID_right","sep_path","bird","noise","left_csv","right_csv"])
+        
+        for index, row in md.iterrows():
+            audio = []
+            mini_dict = {'Left':row['ID_left'],'Right':row['ID_right']}
+            if not os.path.exists(row["sep_path"]+'-'+row['ID_left']+'-source4.wav') or not os.path.exists(row["sep_path"]+'-'+row['ID_right']+'-source4.wav') :
+                continue
+            for i,c in enumerate(mini_dict):
+                p = mini_dict[c]
+                for s in range(1,NSOURCES+1):
+                    input_mix,sample_rate = librosa.load(row["sep_path"]+'-'+p+'-source'+str(s)+'.wav',sr=None)
+                    if  i==0 and s==1:
+                        audio = np.zeros((2,NSOURCES,len(input_mix)))
+                        stats = np.zeros((2,NSOURCES,2))
+                    if len(input_mix) < len(audio[i,s-1,:]):
+                        audio[i,s-1,:len(input_mix)] = input_mix
+                    elif len(input_mix) > len(audio[i,s-1,:]):
+                        audio[i,s-1,:] = input_mix[:len(audio[i,s-1,:])]
+                    else:
+                        audio[i,s-1,:] = input_mix
+                    waveform = librosa.util.normalize(input_mix) 
+                    scores, _, _ = model(waveform)
+                    scores_np = scores.numpy()
+                    scores_birds = scores_np[:,bird_ids]
+                    stats[0,s-1,i] = np.max(scores_birds)
+                    stats[1,s-1,i] = np.mean(scores_birds)
 
-                birds = [row['ID_left'],row['ID_right']]
-                channels = ['left','right']
-                for i in range(2):
-                    bird_events = []
-                    for s,e in zip(start[i], end[i]):
-                        #search with searchsorted for a overlapping interval in i-1 
-                        sidx = np.searchsorted(start[i-1], s)
-                        eidx = np.searchsorted(end[i-1], e)
-                        intersect = 0
-                        if sidx< len(start[i-1]) and start[i-1][sidx] < e and end[i-1][sidx] > s:
-                            intersect = min(end[i-1][sidx], e) - max(start[i-1][sidx], s) 
-                        if eidx<len(end[i-1]) and start[i-1][eidx] < e and end[i-1][eidx] > s:
-                            intersect = min(end[i-1][eidx], e) - max(start[i-1][eidx], s) 
-                        # if intersect:
-                        #     print('overlap {} {} {} {} {} {} {}'.format(s,e,start[i-1][sidx],end[i-1][sidx],start[i-1][eidx],end[i-1][eidx], intersect))
-                        #     import pdb;pdb.set_trace()
-                        if 2*intersect<(e-s) or rms[i][s:e].mean() > rms[i-1][s:e].mean() or np.abs(rms[i][s:e].max() - rms[i-1][s:e].max()) < 0.2: #this thresholding might be tricky
-                            bird_events.append([np.round(s*frames2time,2),np.round(e*frames2time,2),rms[i][s:e].mean()])
-                            # ax.axvspan(s, e, alpha=0.2, color=colors[i])
+            ### at least one prediction is above threshold and its mean is above threshold (more than a single vocalization)
+            if np.any(np.logical_and(stats[0,:,:]>0.9, stats[1,:,:]>0.01)):
+                ### avoid having bird predictions in the majority of tracks - cases where mixit fails
+                if np.all((np.logical_and(stats[0,:,:]>0.9, stats[1,:,:]>0.01)).sum(axis=0)<NSOURCES//2):
+                    pred_bird = np.zeros((2, len(audio[0,0,:])))
+                    pred_noise = np.zeros((2, len(audio[0,0,:])))
+                    for i in range(2):
+                        max_max = stats[0,:,i].max() 
+                        argmax_max = stats[0,:,i].argmax()
+                        for s in range(NSOURCES):
+                            if stats[0,s,i] == max_max:
+                                pred_bird[i,:] += audio[i,s,:]
+                            else:
+                                pred_noise[i,:] += audio[i,s,:]
+                    if WIENER:
+                        pred_bird, pred_noise = wiener_filter(pred_bird, pred_noise)            
+                        
+                    sf.write(os.path.join(conf["save_dir"],'{}-{}-{}-bird.wav'.format(row['timestamp'], row['ID_left'], row['ID_right'])), pred_bird.transpose(), sample_rate, 'PCM_24')
+                    sf.write(os.path.join(conf["save_dir"],'{}-{}-{}-noise.wav'.format(row['timestamp'], row['ID_left'], row['ID_right'])), pred_noise.transpose(), sample_rate, 'PCM_24')
+                    frames2time = 512/sample_rate
+                    rms = librosa.feature.rms(S=librosa.magphase(librosa.stft(pred_bird, window=np.ones, center=False))[0]).squeeze()
+                    rms[0] = librosa.util.normalize(rms[0]) 
+                    rms[1] = librosa.util.normalize(rms[1]) 
+                    rms = scipy.ndimage.uniform_filter1d(rms, size=5)
+                    activity, start, end = [], [], []
+                    for i in range(2):
+                        a, s, e = threshold_activity(rms[i], 0.01 , 0.1)
+                        activity.append(a)
+                        start.append(s)
+                        end.append(e)
 
-                    with open(os.path.join(conf["save_dir"],'{}-{}-{}.csv'.format(row['timestamp'],channels[i],birds[i])), 'w') as f:
-                        write = csv.writer(f)
-                        write.writerow(['start','end','rms'])
-                        write.writerows(bird_events)
-               
-                # plt.legend()
-                # plt.show()
-                # import pdb;pdb.set_trace()
+                    # fig, ax = plt.subplots(figsize=(10,6))
+                    # ax.plot(rms[0], label='left',color='green')
+                    # ax.plot(rms[1], label='right',color='red')
+                    # colors = ['green','red']
 
-                rowf = [row['time'],row['timestamp'], row['ID_left'], row['ID_right'], row["sep_path"],'{}-{}-{}-bird.wav'.format(row['timestamp'], row['ID_left'], row['ID_right']),'{}-{}-{}-noise.wav'.format(row['timestamp'], row['ID_left'], row['ID_right']),'{}-{}-{}.csv'.format(row['timestamp'],'left',birds[0]),'{}-{}-{}.csv'.format(row['timestamp'],'right',birds[1])]
-                filtered_md.loc[len(filtered_md)] = rowf
+                    birds = [row['ID_left'],row['ID_right']]
+                    channels = ['left','right']
+                    for i in range(2):
+                        bird_events = []
+                        for s,e in zip(start[i], end[i]):
+                            #search with searchsorted for a overlapping interval in i-1 
+                            sidx = np.searchsorted(start[i-1], s)
+                            eidx = np.searchsorted(end[i-1], e)
+                            intersect = 0
+                            if sidx< len(start[i-1]) and start[i-1][sidx] < e and end[i-1][sidx] > s:
+                                intersect = min(end[i-1][sidx], e) - max(start[i-1][sidx], s) 
+                            if eidx<len(end[i-1]) and start[i-1][eidx] < e and end[i-1][eidx] > s:
+                                intersect = min(end[i-1][eidx], e) - max(start[i-1][eidx], s) 
+                            # if intersect:
+                            #     print('overlap {} {} {} {} {} {} {}'.format(s,e,start[i-1][sidx],end[i-1][sidx],start[i-1][eidx],end[i-1][eidx], intersect))
+                            #     import pdb;pdb.set_trace()
+                            if 2*intersect<(e-s) or rms[i][s:e].mean() > rms[i-1][s:e].mean() or np.abs(rms[i][s:e].max() - rms[i-1][s:e].max()) < 0.2: #this thresholding might be tricky
+                                bird_events.append([np.round(s*frames2time,2),np.round(e*frames2time,2),rms[i][s:e].mean()])
+                                # ax.axvspan(s, e, alpha=0.2, color=colors[i])
+
+                        with open(os.path.join(conf["save_dir"],'{}-{}-{}.csv'.format(row['timestamp'],channels[i],birds[i])), 'w') as f:
+                            write = csv.writer(f)
+                            write.writerow(['start','end','rms'])
+                            write.writerows(bird_events)
+
+                    # plt.legend()
+                    # plt.show()
+                    # import pdb;pdb.set_trace()
+
+                    rowf = [row['time'],row['timestamp'], row['ID_left'], row['ID_right'], row["sep_path"],'{}-{}-{}-bird.wav'.format(row['timestamp'], row['ID_left'], row['ID_right']),'{}-{}-{}-noise.wav'.format(row['timestamp'], row['ID_left'], row['ID_right']),'{}-{}-{}.csv'.format(row['timestamp'],'left',birds[0]),'{}-{}-{}.csv'.format(row['timestamp'],'right',birds[1])]
+                    filtered_md.loc[len(filtered_md)] = rowf
     
     filtered_md.to_csv(os.path.join(conf["save_dir"],'metadata.csv'), index=False)
 
