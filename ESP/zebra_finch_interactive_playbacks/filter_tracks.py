d6d91e057c2559d4fd5f10cd88b72cbd02fcf60f
Name <miron.marius@gmail.com>
2023-07-06 16:22:11 +0200

    improve detection

--- ESP/zebra_finch_interactive_playbacks/filter_tracks.py
+++ ESP/zebra_finch_interactive_playbacks/filter_tracks.py
@@ -1,5 +1,5 @@
 '''
-python filter_tracks.py --dataset_path /home/jupyter/data/zebra_finch/Pair1/Audio 
+python filter_tracks.py --dataset_path /home/marius/data/zebra_finch/Pair3
 assumes the separation has been done and the files are in the separation folder
 '''
 import os, sys
@@ -32,6 +32,8 @@ from contextlib import redirect_stdout
 NSOURCES = 4
 CLASSES = {97:"Turkey",98:"Gobble",99:"Duck",100:"Quack",101:"Goose",106:"Bird",107:"Bird vocalization, bird call, bird song",108:"Chirp, tweet",109:"Squawk",111:"Coo",112:"Crow",113:"Caw",127:"Frog"}
 WIENER = False
+THRESHOLD_MAX = 0.1
+THRESHOLD_MEAN = 0.001
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -172,11 +174,11 @@ def main(conf):
                     scores_birds = scores_np[:,bird_ids]
                     stats[0,s-1,i] = np.max(scores_birds)
                     stats[1,s-1,i] = np.mean(scores_birds)
-
-            ### at least one prediction is above threshold and its mean is above threshold (more than a single vocalization)
-            if np.any(np.logical_and(stats[0,:,:]>0.9, stats[1,:,:]>0.01)):
-                ### avoid having bird predictions in the majority of tracks - cases where mixit fails
-                if np.all((np.logical_and(stats[0,:,:]>0.9, stats[1,:,:]>0.01)).sum(axis=0)<NSOURCES//2):
+            
+            ### at least one prediction is above threshold or its mean is above threshold (more than a single vocalization)
+            if np.any(np.logical_and(stats[0,:,:]>THRESHOLD_MAX, stats[1,:,:]>THRESHOLD_MEAN)):
+                ### avoid having bird predictions in all of the tracks - cases where mixit fails
+                if np.all((np.logical_and(stats[0,:,:]>THRESHOLD_MAX, stats[1,:,:]>THRESHOLD_MEAN)).sum(axis=0)<NSOURCES-1):
                     pred_bird = np.zeros((2, len(audio[0,0,:])))
                     pred_noise = np.zeros((2, len(audio[0,0,:])))
                     for i in range(2):
@@ -189,11 +191,11 @@ def main(conf):
                                 pred_noise[i,:] += audio[i,s,:]
                     if WIENER:
                         pred_bird, pred_noise = wiener_filter(pred_bird, pred_noise)            
-                        
                     sf.write(os.path.join(conf["save_dir"],'{}-{}-{}-bird.wav'.format(row['timestamp'], row['ID_left'], row['ID_right'])), pred_bird.transpose(), sample_rate, 'PCM_24')
                     sf.write(os.path.join(conf["save_dir"],'{}-{}-{}-noise.wav'.format(row['timestamp'], row['ID_left'], row['ID_right'])), pred_noise.transpose(), sample_rate, 'PCM_24')
+                    
                     frames2time = 512/sample_rate
-                    rms = librosa.feature.rms(S=librosa.magphase(librosa.stft(pred_bird, window=np.ones, center=False))[0]).squeeze()
+                    rms = librosa.feature.rms(S=librosa.magphase(librosa.stft(pred_bird, n_fft=2048, hop_length=512, win_length=2048, window=np.ones, center=False))[0]).squeeze()
                     rms[0] = librosa.util.normalize(rms[0]) 
                     rms[1] = librosa.util.normalize(rms[1]) 
                     rms = scipy.ndimage.uniform_filter1d(rms, size=5)
@@ -201,13 +203,13 @@ def main(conf):
                     for i in range(2):
                         a, s, e = threshold_activity(rms[i], 0.01 , 0.1)
                         activity.append(a)
-                        start.append(s)
-                        end.append(e)
+                        start.append(s-5) # -5 to compensate for the 5 frames window smoothing
+                        end.append(e-5)
 
-                    # fig, ax = plt.subplots(figsize=(10,6))
-                    # ax.plot(rms[0], label='left',color='green')
-                    # ax.plot(rms[1], label='right',color='red')
-                    # colors = ['green','red']
+                    fig, ax = plt.subplots(figsize=(10,6))
+                    ax.plot(rms[0], label='left',color='green')
+                    ax.plot(rms[1], label='right',color='red')
+                    colors = ['green','red']
 
                     birds = [row['ID_left'],row['ID_right']]
                     channels = ['left','right']
@@ -217,30 +219,38 @@ def main(conf):
                             #search with searchsorted for a overlapping interval in i-1 
                             sidx = np.searchsorted(start[i-1], s)
                             eidx = np.searchsorted(end[i-1], e)
-                            intersect = 0
-                            if sidx< len(start[i-1]) and start[i-1][sidx] < e and end[i-1][sidx] > s:
-                                intersect = min(end[i-1][sidx], e) - max(start[i-1][sidx], s) 
-                            if eidx<len(end[i-1]) and start[i-1][eidx] < e and end[i-1][eidx] > s:
-                                intersect = min(end[i-1][eidx], e) - max(start[i-1][eidx], s) 
-                            # if intersect:
-                            #     print('overlap {} {} {} {} {} {} {}'.format(s,e,start[i-1][sidx],end[i-1][sidx],start[i-1][eidx],end[i-1][eidx], intersect))
-                            #     import pdb;pdb.set_trace()
-                            if 2*intersect<(e-s) or rms[i][s:e].mean() > rms[i-1][s:e].mean() or np.abs(rms[i][s:e].max() - rms[i-1][s:e].max()) < 0.2: #this thresholding might be tricky
-                                bird_events.append([np.round(s*frames2time,2),np.round(e*frames2time,2),rms[i][s:e].mean()])
-                                # ax.axvspan(s, e, alpha=0.2, color=colors[i])
+                            if sidx == eidx:
+                                intersect = 0
+                                if sidx< len(start[i-1]) and start[i-1][sidx] < e and end[i-1][sidx] > s:
+                                    intersect = min(end[i-1][sidx], e) - max(start[i-1][sidx], s) 
+                                if eidx<len(end[i-1]) and start[i-1][eidx] < e and end[i-1][eidx] > s:
+                                    intersect = min(end[i-1][eidx], e) - max(start[i-1][eidx], s) 
+                                if intersect:
+                                    print('overlap {} {} {} {} {} {} {}'.format(s,e,start[i-1][sidx],end[i-1][sidx],start[i-1][eidx],end[i-1][eidx], intersect))
+                                    #import pdb;pdb.set_trace()
+                                    if 2*intersect<(e-s) or rms[i][s:e].mean() > rms[i-1][s:e].mean() or np.abs(rms[i][s:e].max() - rms[i-1][s:e].max()) < 0.2: #this thresholding might be tricky
+                                        bird_events.append([np.round(s*frames2time,2),np.round(e*frames2time,2),rms[i][s:e].mean()])
+                                        ax.axvspan(s, e, alpha=0.2, color=colors[i])
+                                else:
+                                    bird_events.append([np.round(s*frames2time,2),np.round(e*frames2time,2),rms[i][s:e].mean()])
+                                    ax.axvspan(s, e, alpha=0.2, color=colors[i])
 
                         with open(os.path.join(conf["save_dir"],'{}-{}-{}.csv'.format(row['timestamp'],channels[i],birds[i])), 'w') as f:
                             write = csv.writer(f)
                             write.writerow(['start','end','rms'])
                             write.writerows(bird_events)
 
-                    # plt.legend()
-                    # plt.show()
+                    plt.legend()
+                    plt.show()
                     # import pdb;pdb.set_trace()
 
                     rowf = [row['time'],row['timestamp'], row['ID_left'], row['ID_right'], row["sep_path"],'{}-{}-{}-bird.wav'.format(row['timestamp'], row['ID_left'], row['ID_right']),'{}-{}-{}-noise.wav'.format(row['timestamp'], row['ID_left'], row['ID_right']),'{}-{}-{}.csv'.format(row['timestamp'],'left',birds[0]),'{}-{}-{}.csv'.format(row['timestamp'],'right',birds[1])]
                     filtered_md.loc[len(filtered_md)] = rowf
-    
+                else:
+                    print("Skipping "+row["sep_path"]+" because mixit was unable to separate (all sources contain birds).")
+            else:
+                print("Skipping "+row["sep_path"]+" because there are no bird predictions above threshold in any of the separations.")
+
     filtered_md.to_csv(os.path.join(conf["save_dir"],'metadata.csv'), index=False)
 
 
