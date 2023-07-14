c3e647b09effa055523be2e4c027125cc28aca35
Name <miron.marius@gmail.com>
2023-07-14 15:30:43 +0200

    add gcp script

---
+++ ESP/zebra_finch_interactive_playbacks/gcp_script.sh
@@ -0,0 +1,14 @@
+python mixit_separation.py --dataset_path /home/marius/data/zebra_finch_interactive_playbacks/Pair1 --model_dir /home/marius/data/bird_mixit_model_checkpoints/
+python filter_tracks.py --dataset_path /home/marius/data/zebra_finch_interactive_playbacks/Pair1
+python mixit_separation.py --dataset_path /home/marius/data/zebra_finch_interactive_playbacks/Pair2 --model_dir /home/marius/data/bird_mixit_model_checkpoints/
+python filter_tracks.py --dataset_path /home/marius/data/zebra_finch_interactive_playbacks/Pair2
+python mixit_separation.py --dataset_path /home/marius/data/zebra_finch_interactive_playbacks/Pair3 --model_dir /home/marius/data/bird_mixit_model_checkpoints/
+python filter_tracks.py --dataset_path /home/marius/data/zebra_finch_interactive_playbacks/Pair3
+python mixit_separation.py --dataset_path /home/marius/data/zebra_finch_interactive_playbacks/Pair4 --model_dir /home/marius/data/bird_mixit_model_checkpoints/
+python filter_tracks.py --dataset_path /home/marius/data/zebra_finch_interactive_playbacks/Pair4
+python mixit_separation.py --dataset_path /home/marius/data/zebra_finch_interactive_playbacks/Pair5 --model_dir /home/marius/data/bird_mixit_model_checkpoints/
+python filter_tracks.py --dataset_path /home/marius/data/zebra_finch_interactive_playbacks/Pair5
+python mixit_separation.py --dataset_path /home/marius/data/zebra_finch_interactive_playbacks/Pair6 --model_dir /home/marius/data/bird_mixit_model_checkpoints/
+python filter_tracks.py --dataset_path /home/marius/data/zebra_finch_interactive_playbacks/Pair6
+python mixit_separation.py --dataset_path /home/marius/data/zebra_finch_interactive_playbacks/Pair7 --model_dir /home/marius/data/bird_mixit_model_checkpoints/
+python filter_tracks.py --dataset_path /home/marius/data/zebra_finch_interactive_playbacks/Pair7
